f2e2a48fcc93f8202219ccbb1a3613ad6c251af3
friedolin.foerder <friedolin@steuerbot.com>
2019-11-26 17:31:13 +0100

    fixed html

--- injectedHtml/index.js
+++ injectedHtml/index.js
@@ -1,24 +1,44 @@
-export default ({script, backgroundColor = '#ffffff'}) =>
-  `<html>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-    * {
-      margin:0;
-      padding:0;
-    }
-    
-    body {
-      background: ${backgroundColor};
-    }
+export default ({
+  script,
+  backgroundColor = '#ffffff',
+  ratio = 1,
+  subtitle = '&nbsp;',
+}) => `<html>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+  * {
+    margin:0;
+    padding:0;
+  }
+  
+  body {
+    background: ${backgroundColor};
+  }
 
-    canvas {
-      position:absolute;
-      transform:translateZ(0);
-    }
+  canvas {
+    position:absolute;
+    transform:translateZ(0);
+  }
+  
+  #line {
+    position: absolute;
+    left: ${ratio * 24}px;
+    right: ${ratio * 24}px;
+    bottom: ${ratio * 24}px;
+    border-top: ${ratio * 2}px solid #999;
+    font-size: ${ratio * 12}px;
+    padding: ${ratio * 8}px;
+    font-family: "SF Pro Text", "Roboto", "Arial";
+    color: #999;
+    text-align: center;
+  }
 
-    </style>
-    <body>
-      <canvas></canvas>
-      <script>${script}</script>
-    </body>
-  </html>`;
+  </style>
+  <body>
+    <div id="line">
+      ${subtitle}
+    </div>
+    <canvas></canvas>
+    <script>${script}</script>
+  </body>
+</html>`;
